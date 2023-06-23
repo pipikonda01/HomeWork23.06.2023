6223b8eae25d6f70a488fa3aa61243faafd49001
kyninja <ilya.stasevich.98@gmail.com>
2023-06-23 18:39:18 +0300

    Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите пятизначное число: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num < 10000 || num > 99999)
+        {
+            Console.WriteLine("Ошибка.Ваше число не пятизначное.");
+            Environment.Exit(0);
+        }
+
+        if (num < 0)
+        {
+            Console.WriteLine("Ваше число не полиндром.");
+        }
+
+        string num_str = num.ToString();
+
+        if (num_str[0] + num_str[1] == num_str[4] + num_str[3])
+        {
+            Console.WriteLine("Ваше число полиндром");
+        }
+        else
+        {
+            Console.WriteLine("Ваше число не полиндром");
+        }
+    }
+}
